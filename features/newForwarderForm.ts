377bad02bd4143bb6fe6cb4bd236f7df77d089af
Dominic Grimaldi <self@grimoutlook.dev>
2025-11-24 08:53:17 -0600

    wip!

--- features/newForwarderForm.ts
+++ features/newForwarderForm.ts
@@ -1,39 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
 
 export interface NewForwarderFormState {
-  new_forwarder: string | null,
-  forward_to: string | null,
+  new_forwarder: string | null;
+  forward_to: string | null;
 }
 
 const initialState: NewForwarderFormState = {
   new_forwarder: null,
   forward_to: null,
-}
+};
 
 export const newForwarderForm = createSlice({
-  name: 'newForwarderForm',
+  name: 'new_forwarder',
   initialState,
   reducers: {
     setNewForwarder: (state, action: PayloadAction<string>) => {
       // TODO: Validate that the given string is a valid email
-      state.new_forwarder = action.payload
+      state.new_forwarder = action.payload;
     },
 
     setForwardTo: (state, action: PayloadAction<string>) => {
       // TODO: Validate that the given string is a valid email
-      state.forward_to = action.payload
+      state.forward_to = action.payload;
     },
 
     resetForm: (state) => {
-      state = initialState
-    }
+      state = initialState;
+    },
   },
   selectors: {
     getNewForwarder: (state) => state.new_forwarder,
     getForwardTo: (state) => state.forward_to,
-  }
-})
+  },
+});
 
-export const { setNewForwarder, setForwardTo, resetForm } = newForwarderForm.actions
-export const { getNewForwarder, getForwardTo } = newForwarderForm.selectors
+export const { setNewForwarder, setForwardTo, resetForm } = newForwarderForm.actions;
+export const { getNewForwarder, getForwardTo } = newForwarderForm.selectors;
--- lib/store.ts
+++ lib/store.ts
@@ -8,15 +8,19 @@ import { setup } from '@/features/setup';
 import { settings } from '@/features/settings';
 import { directadminApi } from '@/features/directadminApi';
 
+// NOTE: `redux-persist` tries to use keys with `:` in them which is not
+// allowed. Need to convert them to use a different character.
+const sanitizedKey = (key: string) => key.replace(/:/g, '_');
 const secureStorage = {
   async setItem(key: string, value: string) {
-    await SecureStore.setItemAsync(key, value);
+    console.debug('Setting item with key: ', key);
+    await SecureStore.setItemAsync(sanitizedKey(key), value);
   },
   async getItem(key: string) {
-    return await SecureStore.getItemAsync(key);
+    return await SecureStore.getItemAsync(sanitizedKey(key));
   },
   async removeItem(key: string) {
-    await SecureStore.deleteItemAsync(key);
+    await SecureStore.deleteItemAsync(sanitizedKey(key));
   },
 };
 
@@ -35,11 +39,12 @@ const persistConfig = {
 const persistedSettings = persistReducer(persistConfig, settings.reducer);
 const persistedSetup = persistReducer(securePersistConfig, setup.reducer);
 
-const rootReducer = combineReducers([
-  combineSlices(directadminApi, newForwarderForm),
-  persistedSettings,
-  persistedSetup,
-]);
+const rootReducer = combineReducers({
+  directadmin_api: directadminApi.reducer,
+  new_forwarder: newForwarderForm.reducer,
+  settings: persistedSettings,
+  setup: persistedSetup,
+});
 
 export const store = configureStore({
   /*
